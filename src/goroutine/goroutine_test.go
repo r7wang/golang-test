6de0aded708e4ff7e80dc7dc4ac37f7ee4aba8ba
Ray Wang <ray.wang@kik.com>
2019-09-24 11:35:39 -0400

    Added some goroutine tests.

---
+++ src/goroutine/goroutine_test.go
@@ -0,0 +1,32 @@
+package goroutine
+
+import (
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestWaitOnGoroutine(t *testing.T) {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		t.Log("Goroutine")
+	}()
+	wg.Wait()
+}
+
+func TestGoroutineClosure(t *testing.T) {
+	val := 0
+	for goroutineIdx := 0; goroutineIdx < 5; goroutineIdx++ {
+		go func() {
+			time.Sleep(time.Millisecond * 5)
+			t.Log("Goroutine")
+			val++
+		}()
+	}
+	for waitIdx := 0; waitIdx < 10; waitIdx++ {
+		t.Logf("Current value: %d", val)
+		time.Sleep(time.Millisecond * 1)
+	}
+}
